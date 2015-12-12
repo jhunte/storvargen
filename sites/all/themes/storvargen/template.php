ad8862bcd3c0865b7e4d6a330a5ab3cde1203256
Jess Hunte <jess.hunte@gmail.com>
2015-12-12 13:38:32 +0000

    Cleaning up user login and view pages, as well as adding css for the update schedule block

--- sites/all/themes/storvargen/template.php
+++ sites/all/themes/storvargen/template.php
@@ -18,3 +18,26 @@ function storvargen_menu_link(array $variables) {
 //dvm($variables['element']);
   return theme_menu_link($variables);
 }
+
+function storvargen_preprocess_block(&$vars) {
+	if($vars['block']->module == 'block') {
+		$info = block_custom_block_get($vars['block']->delta);
+
+		$description = seoUrl($info['info']);
+
+		$vars['classes_array'][] = $description;
+	}
+
+}
+
+function seoUrl($string) {
+    //Lower case everything
+    $string = strtolower($string);
+    //Make alphanumeric (removes all other characters)
+    $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
+    //Clean up multiple dashes or whitespaces
+    $string = preg_replace("/[\s-]+/", " ", $string);
+    //Convert whitespaces and underscore to dash
+    $string = preg_replace("/[\s_]/", "-", $string);
+    return $string;
+}
