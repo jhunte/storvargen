c941463f114cdace7c3d825d41dadeb4f50004a6
Jess Hunte <jess.hunte@gmail.com>
2015-12-12 15:13:34 +0000

    Testing out view theme suggestion

--- sites/all/themes/storvargen/template.php
+++ sites/all/themes/storvargen/template.php
@@ -30,6 +30,14 @@ function storvargen_preprocess_block(&$vars) {
 
 }
 
+function storvargen_preprocess_page(&$variables) {
+  if (isset(views_get_page_view())) {
+    // If the content type's machine name is "my_machine_name" the file
+    // name will be "page--my-machine-name.tpl.php".
+    $variables['theme_hook_suggestions'][] = 'page__view';
+  }
+}
+
 function seoUrl($string) {
     //Lower case everything
     $string = strtolower($string);
