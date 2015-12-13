5a1b5ff336d9d87a2e112d24e6b4a9c917795f4a
Jess Hunte <jess.hunte@gmail.com>
2015-12-13 00:47:41 +0000

    Testing out title rendering

--- sites/all/themes/storvargen/templates/node--encyclopedia-entry.tpl.php
+++ sites/all/themes/storvargen/templates/node--encyclopedia-entry.tpl.php
@@ -78,7 +78,7 @@
  * @see template_process()
  */
 ?>
-<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> content-wrapper clearfix"<?php print $attributes; ?>>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <div class="content clearfix"<?php print $content_attributes; ?>>
     <?php
